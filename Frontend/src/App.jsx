054aeae58ea46d68d25f37c3f0d0a43c51bfe199
ParthN242 <parthnandani3236@gmail.com>
2024-03-20 13:06:10 +0530

    Change url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,7 +11,7 @@ import React, { getUserDetailAsync } from "./Redux/User/UserDetailSlice";
 import SignIn from "./Pages/Auth/SignIn";
 import SignUp from "./Pages/Auth/SignUp";
 
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL_0;
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 
