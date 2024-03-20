91f191f2f847aa7aeef9c78981302855dcf85b97
ParthN242 <parthnandani3236@gmail.com>
2024-03-20 13:23:35 +0530

    JWT token option change

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -37,6 +37,8 @@ exports.loginUser = async (req, res, next) => {
     const option = {
       expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
       httpOnly: true,
+      sameSite: "none",
+      secure: "false",
     };
 
     res.cookie("token", token, option);
